0379af085e7450799909e441cc7ecca5d72d232e
Jacob O. Smith <122314435+jacobsmith95@users.noreply.github.com>
2024-07-08 22:20:30 -0700

    added pressure function and finished basic plotting

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-import matplotlib as mp
-from DynamicPressure import DynamicPressure as Q
-from DynamicPressure import MaxQ
+import numpy as np
+import matplotlib.pyplot as plt
+from DynamicPressure import *
+from ModelRocket import Rocket
 
 
-def main():
+#r1 = Rocket(30000, 2256, 1.0)
+
+def get_pressure(time):
     """"""
+    r1.change_time(time)
+    r1.calculate_mass()
+    r1.change_ratio()
+    r1.calculate_acc()
+    r1.calculate_vel()
+    velocity = r1.get_velocity()
+    r1.calculcate_alt()
+    altitude = r1.get_altitude()
+    pressure = dynamic_pressure(velocity, altitude)
+    dynamic_pressure_list.append(pressure)
+    #r1.change_time(time)
+    return pressure
 
-
+r1 = Rocket(5000000, 74400, 1.0)
+x = np.arange(0.0, 10000.0, .1)
+dynamic_pressure_list = []
+for num in x:
+    get_pressure(num)
+fig, ax = plt.subplots()
+ax.plot(x, dynamic_pressure_list)
+plt.show()
