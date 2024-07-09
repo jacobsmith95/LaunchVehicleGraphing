fe7e28e6dbf9e7cc5daa106f24dfbca26c05fff1
Jacob O. Smith <122314435+jacobsmith95@users.noreply.github.com>
2024-07-08 22:19:56 -0700

    added mass reduction and mdot value

--- ModelRocket.py
+++ ModelRocket.py
@@ -4,27 +4,35 @@ import numpy as np
 class Rocket():
     """"""
 
-    def __init__(self, mass: float, thrust: float):
+    def __init__(self, mass: float, thrust: float, mdot: float):
         """"""
         self.mass   = mass
         self.weight = mass*(9.80665)
         self.thrust = thrust
         self.ratio  = self.thrust/self.weight
+        self.mdot   = mdot
         self.vel    = 0.0
         self.acc    = 0.0
         self.time   = 0.0
+        self.alt    = 0.0
 
-    def reduce_mass(self, mass_change: float):
+    def calculate_mass(self):
         """"""
-        self.mass -= mass_change
+        self.mass = self.mass - (self.mdot*self.time)
+        if self.mass <= 0.0:
+            self.mass = 0.1
 
     def calculate_vel(self):
         """"""
         self.vel = self.acc*self.time
 
-    def calculate_acc(self, acceleration: float):
+    def calculate_acc(self):
         """"""
         self.acc = self.ratio/self.mass
+
+    def calculcate_alt(self):
+        """"""
+        self.alt = (0.5)*(self.acc)*(self.time**2)
 
     def change_time(self, new_time: float):
         """"""
@@ -34,6 +42,10 @@ class Rocket():
         """"""
         self.ratio = self.thrust/self.weight
 
-    def return_velocity(self):
+    def get_velocity(self):
         """"""
         return self.vel
+    
+    def get_altitude(self):
+        """"""
+        return self.alt
