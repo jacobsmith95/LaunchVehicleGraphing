d1fb7972f31bf1246154396fc41490fba2c4f816
Jacob O. Smith <122314435+jacobsmith95@users.noreply.github.com>
2024-07-08 20:40:53 -0700

    added weight, thrust, thrust-to-weight ratio, and time

--- ModelRocket.py
+++ ModelRocket.py
@@ -4,21 +4,36 @@ import numpy as np
 class Rocket():
     """"""
 
-    def __init__(self, mass: float):
+    def __init__(self, mass: float, thrust: float):
         """"""
-        self.mass = mass
-        self.vel  = 0.0
-        self.acc  = 0.0
+        self.mass   = mass
+        self.weight = mass*(9.80665)
+        self.thrust = thrust
+        self.ratio  = self.thrust/self.weight
+        self.vel    = 0.0
+        self.acc    = 0.0
+        self.time   = 0.0
 
     def reduce_mass(self, mass_change: float):
         """"""
         self.mass -= mass_change
 
-    def change_vel(self, velocity: float):
+    def calculate_vel(self):
         """"""
-        self.vel = velocity
+        self.vel = self.acc*self.time
 
-    def change_acc(self, acceleration: float):
+    def calculate_acc(self, acceleration: float):
         """"""
-        self.acc = acceleration
-        
+        self.acc = self.ratio/self.mass
+
+    def change_time(self, new_time: float):
+        """"""
+        self.time = new_time
+
+    def change_ratio(self):
+        """"""
+        self.ratio = self.thrust/self.weight
+
+    def return_velocity(self):
+        """"""
+        return self.vel
